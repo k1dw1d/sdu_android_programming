8aad6742a38df9b02600345076c97e49b05d0111
Jakob Frederiksen <jafre13@student.sdu.dk>
2018-03-05 14:53:57 +0100

    added start of exercise

---
+++ Fragment_Exercise_1/app/src/main/java/sdu/android/programming/com/fragment_exercise_1/WhiteFragment.java
@@ -0,0 +1,34 @@
+package sdu.android.programming.com.fragment_exercise_1;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jakob on 05/03/2018.
+ */
+
+public class WhiteFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_white,container,false);
+        return v;
+    }
+
+    @Override
+    public void onPause() {
+        //DO NOTHING
+        super.onPause();
+    }
+}
