4490dfd5b7e541741072252028aed87a586a489f
Jakob Frederiksen <jafre13@student.sdu.dk>
2018-03-14 13:06:11 +0100

    added some UI and Animations

--- Threading_Exercise/app/src/main/java/com/example/slapocolypse/threading_exercise/MainActivity.java
+++ Threading_Exercise/app/src/main/java/com/example/slapocolypse/threading_exercise/MainActivity.java
@@ -1,19 +1,22 @@
 package com.example.slapocolypse.threading_exercise;
 
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.widget.TextSwitcher;
 
 import java.io.IOException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    @BindView(R.id.joke_holder)
+    TextSwitcher textSwitcher;
 
     String url = "http://api.icndb.com/";
 
@@ -26,7 +29,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.joke_holder);
+        ButterKnife.bind(this);
+
+        textSwitcher.setInAnimation(this, android.R.anim.slide_in_left);
+        textSwitcher.setOutAnimation(this, android.R.anim.slide_out_right);
 
         Retrofit retrofit = new Retrofit.Builder().baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -36,37 +42,35 @@ public class MainActivity extends AppCompatActivity {
         workerThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                while (running){
+                while (running) {
                     Call<Joke> joke = jokeService.randomJoke();
                     try {
-                        final String joke_txt = joke.execute().body().getValue().getJoke();
-                        textView.post(new Runnable() {
+                        final String joke_txt = Html.fromHtml(joke.execute().body().getValue().getJoke()).toString();
+                        textSwitcher.post(new Runnable() {
                             @Override
                             public void run() {
-                                textView.setText(joke_txt);
+                                textSwitcher.setText(joke_txt);
                             }
                         });
-
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                     try {
-                        Thread.sleep(10000);
+                        Thread.sleep(5000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-
-
             }
         });
+
         workerThread.start();
     }
 
     @Override
     protected void onDestroy() {
+
         super.onDestroy();
-        running = false;
         try {
             workerThread.join();
         } catch (InterruptedException e) {
