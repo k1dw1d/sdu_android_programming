5b951d9c00671fbd95a76c51cea7beb4d752ba51
Jakob <jafre13@student.sdu.dk>
2018-03-14 16:49:09 +0100

    a few comments

--- Threading_Exercise/app/src/main/java/com/example/slapocolypse/threading_exercise/MainActivity.java
+++ Threading_Exercise/app/src/main/java/com/example/slapocolypse/threading_exercise/MainActivity.java
@@ -15,37 +15,56 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Example of using Butterknife in your application
     @BindView(R.id.joke_holder)
     TextSwitcher textSwitcher;
 
-    String url = "http://api.icndb.com/";
+    //Retrofit stuff
+    Retrofit retrofit;
+
+    final String url = "http://api.icndb.com/";
 
     JokeService jokeService;
 
+    //Thread Stuff
     Thread workerThread;
+
+        //Semaphore for keeping track of thread
     boolean running = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Activate butterknife, binding to this activity
         ButterKnife.bind(this);
 
+        //Declares standard animations for textSwitcher
         textSwitcher.setInAnimation(this, android.R.anim.slide_in_left);
         textSwitcher.setOutAnimation(this, android.R.anim.slide_out_right);
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(url)
+        //Instantiates Retrofrit
+        retrofit = new Retrofit.Builder().baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+
+        //create retrofit instance of JokeService
         jokeService = retrofit.create(JokeService.class);
 
+        //Create a thread
         workerThread = new Thread(new Runnable() {
             @Override
             public void run() {
+                //Make sure the thread is still supposed to run.
                 while (running) {
+                    //Use jokeservice to get a joke
                     Call<Joke> joke = jokeService.randomJoke();
                     try {
+                        //Convert joke to string, formatting it, avoiding html escape characters
                         final String joke_txt = Html.fromHtml(joke.execute().body().getValue().getJoke()).toString();
+
+                        //new runnable for changing text in textswitcher
                         textSwitcher.post(new Runnable() {
                             @Override
                             public void run() {
@@ -55,8 +74,9 @@ public class MainActivity extends AppCompatActivity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    //Have thread sleep for 10 seconds (10.000 ms)
                     try {
-                        Thread.sleep(8000);
+                        Thread.sleep(10000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -64,11 +84,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //start the thread
         workerThread.start();
     }
 
     @Override
     protected void onDestroy() {
+        // Stop running the thread
         running = false;
         super.onDestroy();
         try {
